358236d5de78c9150a3af3c3b3e4fe37cb576dd2
Grinch27 <Grinch27@outlook.com>
2024-03-15 22:24:29 +0800

    240315 diy2 test2: fix uhttpd nginx

--- config/immortalwrt-master/diy-part2.sh
+++ config/immortalwrt-master/diy-part2.sh
@@ -14,6 +14,14 @@ CONFIG_PACKAGE_uhttpd=n
 CONFIG_PACKAGE_uhttpd-mod-ubus=n
 " >> .config
 
+# filetransfer (depend on uhttpd)
+echo "
+CONFIG_DEFAULT_luci-app-filetransfer=n
+CONFIG_MODULE_DEFAULT_luci-app-filetransfer=n
+CONFIG_PACKAGE_luci-app-filetransfer=n
+CONFIG_PACKAGE_luci-i18n-filetransfer-zh-cn=n
+" >> .config
+
 # samba4
 echo "
 CONFIG_MODULE_DEFAULT_luci-app-samba4=n
@@ -52,7 +60,20 @@ CONFIG_PACKAGE_luci-i18n-usb-printer-zh-cn=n
 # ----- install -----
 # nginx Luci
 echo "
+# nginx-ssl includes nginx
 CONFIG_PACKAGE_luci-ssl-nginx=y
+
+CONFIG_PACKAGE_nginx=n
+CONFIG_PACKAGE_nginx-ssl=y
+
+CONFIG_PACKAGE_nginx-all-module=n
+CONFIG_PACKAGE_nginx-mod-luci=y
+CONFIG_PACKAGE_nginx-mod-luci-ssl=y
+
+CONFIG_PACKAGE_nginx-util=y
+CONFIG_PACKAGE_nginx-ssl-util=y
+CONFIG_PACKAGE_nginx-ssl-util-nopcre=n
+
 " >> .config
 
 # dos2unix unix2dos
